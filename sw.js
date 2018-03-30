d7e6dc208bc39cf6e2438e4e6ce60ae4426dd1f1
laczor <laczor@gmail.com>
2018-03-30 02:19:08 +0100

    feat: fetch event added to the service worker

--- sw.js
+++ sw.js
@@ -40,3 +40,15 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
